d777d0621bc22b87a9a68f5a601bf17c8e5df36a
eokon <45616495+euokon@users.noreply.github.com>
2022-07-08 11:34:10 +0100

    first push

---
+++ ApplicationProject/Entities/ProjectEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationProject.Entities
+{
+    public class ProjectData
+    {
+        public string ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime ProjectStartDate { get; set; }
+        public DateTime ExpectedEndDate { get; set; }
+        public DateTime? ProjectEndDate { get; set; }
+        public char CompletionStatus { get; set; }
+        public double BusinessDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? DateModified { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+
+
+}
